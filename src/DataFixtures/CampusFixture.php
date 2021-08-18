4264d9c2dc44b803ab62092a02d866f29a5ccd27
Alexandre RUIZ <alexandre.ruiz83@gmail.com>
2021-08-18 09:56:33 +0200

    Creation Fixture CAMPUS

---
+++ src/DataFixtures/CampusFixture.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Campus;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker ;
+
+class CampusFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 20; $i++) {
+            $campus = new Campus();
+            $campus->setNom($faker->city);
+            $manager->persist($campus);
+        }
+
+        $manager->flush();
+    }
+}
